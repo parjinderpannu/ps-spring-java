3e4f9ebd146fdc279fa73f73bcc54c1aa48d90c6
Parjinder Pannu <ppannu@cisco.com>
2018-02-03 19:53:39 -0800

    Spring anno appConfig autowired

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,16 +11,16 @@ import com.pluralsight.service.CustomerServiceImpl;
 @ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
-	@Bean(name = "customerService")
-	public CustomerService getCustomerService() {
-//		CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
-		CustomerServiceImpl service = new CustomerServiceImpl();
-//		service.setCustomerRepository(getCustomerRepository());
-		return service;
-	}
+//	@Bean(name = "customerService")
+//	public CustomerService getCustomerService() {
+////		CustomerServiceImpl service = new CustomerServiceImpl(getCustomerRepository());
+//		CustomerServiceImpl service = new CustomerServiceImpl();
+////		service.setCustomerRepository(getCustomerRepository());
+//		return service;
+//	}
 	
-	@Bean(name = "customerRepository")
-	public CustomerRepository getCustomerRepository() {
-		return new HibernateCustomerRepositoryImpl();
-	}
+//	@Bean(name = "customerRepository")
+//	public CustomerRepository getCustomerRepository() {
+//		return new HibernateCustomerRepositoryImpl();
+//	}
 }
